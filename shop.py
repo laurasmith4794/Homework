73771ddece48f67044250f77049c35078c2521ea
laurasmith4794 <137213894+laurasmith4794@users.noreply.github.com>
2023-06-28 20:45:07 +0100

    shop.py

--- shop.py
+++ shop.py
@@ -1,16 +1,17 @@
 prices = {"top": 50, "trousers": 75, "dress": 150}
 balance = 100
 
+
 class ThreeFailedAttempts(Exception):
     pass
 
-def purchased(item, balance):
+def purchase(item, balance):
     if balance >= prices.get(item):
         return True
     else:
         return False
 
-def goodbye_greeting(item):
+def goodbye(item):
     print(f"Here’s your {item}!\n")
     print("Thanks for coming to the shop. Come again!")
     exit(0)
@@ -24,16 +25,15 @@ def retry_purchase(item, balance, attempts=1):
             balance += float(input("How much?"))
         except ValueError:
             raise ValueError("Invalid input, exiting store")
-    if purchased(item, balance):
-        goodbye_greeting(item)
+    if purchase(item, balance):
+        goodbye(item)
     else:
         attempts += 1
         retry_purchase(item, balance, attempts)
 
-
-def greeting():
-    print("Welcome to the shop!")
-    print("We currently have the following items:")
+def welcome():
+    print("Welcome to the clothes shop!")
+    print("We have the following items available:")
     for item in prices:
         print(f"{item} - £{prices.get(item)}")
 
@@ -42,11 +42,9 @@ def greeting():
     if choice == "exit":
         exit(0)
     elif choice in prices.keys():
-        if purchased(choice, balance):
-            goodbye_greeting(choice)
+        if purchase(choice, balance):
+            goodbye(choice)
         else:
             retry_purchase(choice, balance)
     else:
         raise ValueError("Not a valid answer, please return and try again")
-
-greeting()
